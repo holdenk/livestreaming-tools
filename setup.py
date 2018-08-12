5b26e7219b34dce38960937eb7f886ab36f4ce35
Holden Karau <holden@pigscanfly.ca>
2018-08-11 23:21:57 -0700

    Add a setup file with the requirements

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+VERSION = '0.0.1'
+
+setup(
+    name='livestreaming-tools',
+    version=VERSION,
+    author='Holden Karau',
+    author_email='holden@pigscanfly.ca',
+    # Copy the shell script into somewhere likely to be in the users path
+    packages=find_packages(),
+    install_requires=[
+        'google-api-python-client',
+        'google-auth',
+        'google-auth-oauthlib',
+        'google-auth-httplib2',
+        'python-twitch-client',
+        'pytz',
+        'bufferapp',
+    ],
+    test_requires=[
+        'nose==1.3.7',
+        'coverage>3.7.0',
+        'unittest2>=1.0.0',
+    ],
+)
